b65432fd4ce8d37db7ffaba53e6159914292b6ce
にゃんぴょう <nanka.hamilton09@gmail.com>
2022-12-01 14:07:42 +0900

    GoogleDataAPIによるrunReportの追加

---
+++ _data/gadata.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const metadata = require("./metadata.json");
+const propertyId = metadata.googlePropertyId;
+
+// Imports the Google Analytics Data API client library.
+const {BetaAnalyticsDataClient} = require('@google-analytics/data');
+
+// Using a default constructor instructs the client to use the credentials
+// specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
+const analyticsDataClient = new BetaAnalyticsDataClient();
+
+// Runs a simple report.
+module.exports = async function () {
+  const [response] = await analyticsDataClient.runReport({
+    property: `properties/${propertyId}`,
+    dateRanges: [
+      {
+        startDate: "2022-11-27",
+        endDate: "today",
+      },
+    ],
+    metrics: [
+      {
+        name: "eventCount",
+      },
+    ],
+    dimensions: [
+      {
+        name: "eventName",
+      },
+    ],
+    dimensionFilter: {
+      filter: {
+        fieldName: "eventName",
+        inListFilter: {
+          values: [
+            "https://omeme-no-me.com/posts/20221116/"
+          ]
+        },
+      },
+    },
+    limit: 1,
+  });
+
+  // console.log('Report result:');
+  response.rows.forEach(row => {
+    // console.log(`${propertyId}, ${row.metricValues[0].value}`);
+    return row.metricValues[0].value;
+  });
+};
